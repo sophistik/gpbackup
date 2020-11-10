ac84cadb41c97b0f52e51ebdd57aa05ee198bbe4
Kevin Yeap <kyeap@vmware.com>
2020-11-09 23:06:50 -0600

    Attach GPDB 7+ child tables to their root
    In GPDB 7+, we no longer have large DDL commands to create a partition
    table and its leaf partitions. Each leaf partition must be created
    individually (at the moment) and attached to the root or subroot
    partition. To do this, we must query for some simple partition
    information and construct the SQL command to attach the partition. This
    SQL command should be printed immediately after the CREATE TABLE
    statement of the leaf partition.
    
    Authored-by: Kevin Yeap <kyeap@vmware.com>

--- backup/predata_relations.go
+++ backup/predata_relations.go
@@ -236,6 +236,12 @@ func PrintPostCreateTableStatements(metadataFile *utils.FileWithByteCount, toc *
 				utils.MakeFQN(alteredPartitionRelation.OldSchema, alteredPartitionRelation.Name), alteredPartitionRelation.NewSchema))
 	}
 
+	attachInfo := table.AttachPartitionInfo
+	if (attachInfo != AttachPartitionInfo{}) {
+		statements = append(statements,
+			fmt.Sprintf("ALTER TABLE ONLY %s ATTACH PARTITION %s %s;", attachInfo.Parent, attachInfo.Relname, attachInfo.Expr))
+	}
+
 	PrintStatements(metadataFile, toc, table, statements)
 }
 
